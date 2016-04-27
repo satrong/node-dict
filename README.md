5da867316219842dea1552f3c2d89659e8b52cfd
satrong <ck-sa@github.com>
2016-04-27 10:51:52 +0800

    添加install.js，可将dict放到全局命令；修改说明文档；

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dict
-打开cmd/shell输入命令即可翻译，翻译结果基于nodejs爬虫获取
+# 安装方法
+1. 下载源码；
+2. 安装nodejs（4.0+）；
+3. 在dict项目根目录执行`node install.js`（此步骤是将`dict`放到全局环境）
+
+# 使用方法
+1. 打开cmd/shell；
+2. 输入`dict hello`
---
+++ install.js
@@ -0,0 +1,48 @@
+var cp = require("child_process");
+var path = require("path");
+var fs = require("fs");
+var color = require('bash-color');
+var npmRoot = cp.execSync("npm -g root", {
+    encoding: 'utf8'
+});
+var isWin = /win/i.test(process.platform);
+
+fs.writeFile(path.join(npmRoot, '../dict' + (isWin ? '.cmd' : '')), fileContent(__dirname), function(err) {
+    if (err) throw err;
+    console.log("dict 已成为全局命令，您现在可以执行命令：");
+    console.log(color.green("  dict hello"));
+    console.log("即可翻译hello");
+});
+
+function fileContent(projectRoot) {
+    projectRoot = path.join(projectRoot, 'index.js');
+    var s;
+    if (isWin) {
+        s = `@IF EXIST "%~dp0\\node.exe" (
+  "%~dp0\\node.exe"  "${projectRoot}" %*
+) ELSE (
+  @SETLOCAL
+  @SET PATHEXT=%PATHEXT:;.JS;=;%
+  node  "${projectRoot}" %*
+)
+        `;
+    } else if (/linux/i.test(process.platform)) {
+        s = `bin/sh
+basedir=$(dirname "$(echo "$0" | sed -e 's,\\\\,/,g')")
+
+case \`uname\` in
+    *CYGWIN*) basedir=\`cygpath -w "$basedir"\`;;
+esac
+
+if [ -x "$basedir/node" ]; then
+  "$basedir/node"  "${projectRoot}" "$@"
+  ret=$?
+else
+  node  "${projectRoot}" "$@"
+  ret=$?
+fi
+exit $ret
+        `;
+    }
+    return s;
+}
