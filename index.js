3d515bac6e7bc745fa5dd99a4170640b9cd734a2
satrong <satrong@163.com>
2018-07-28 19:16:16 +0800

    添加输出版本号

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 const word = encodeURI(Array.prototype.slice.call(process.argv, 2).join(' '));
 const ora = require('ora');
+const dictList = ["bing", "youdao"]; /// 对应lib文件夹的文件名
 
-var dictList = ["bing", "youdao"]; /// 对应lib文件夹的文件名
+if (['-v', '--version'].indexOf(word.toLowerCase()) > -1) {
+	console.log(require('./package.json').version);
+	return;
+}
 
 (async () => {
 	const spinner = ora().start();
