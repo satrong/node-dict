d1b946358c4f1f7e1eca696faaca6482ccd53b4d
satrong <satrong@163.com>
2018-07-28 19:06:30 +0800

    修复不能查询的bug
    修改依赖包
    修改抓取页面方法

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-var request = require("request");
-var cheerio = require("cheerio");
-var color = require('bash-color');
-var out = process.stdout;
-var word = encodeURI(Array.prototype.slice.call(process.argv, 2).join(' '));
+
+const word = encodeURI(Array.prototype.slice.call(process.argv, 2).join(' '));
+const ora = require('ora');
 
 var dictList = ["bing", "youdao"]; /// 对应lib文件夹的文件名
-(function crawl(i) {
-	var dotCount = 0;
-	var option = require('./lib/' + dictList[i]);
-	var f = setInterval(function() {
-		++dotCount;
-		if (dotCount > 3) {
-			dotCount = 1;
-		}
-		out.clearLine();
-		out.cursorTo(0);
-		out.write(color.green('正在使用 ' + option.name + ' 查询' + '.'.repeat(dotCount)));
-	}, 1000);
-	request({
-		url: option.url(word),
-		headers: option.headers || {},
-		timeout: 5000
-	}, function(error, response, body) {
-		clearInterval(f);
-		out.clearLine();
-		out.cursorTo(0);
-		if (!error && response.statusCode == 200) {
-			var $ = cheerio.load(body);
-			var data = option.action($);
-			if (typeof data === 'string') {
-				i < dictList.length - 1 ? crawl(++i) : console.log(color.cyan(data));
-			} else {
-				data.map(function(item, index) {
-					if (index === 0) {
-						console.log(color.wrap(item, color.colors.BLUE, color.styles.hi_background));
-					} else {
-						console.log(color.wrap(item[0], color.colors.YELLOW, color.styles.background), item[1]);
-					}
-				});
+
+(async () => {
+	const spinner = ora().start();
+	for (let i = 0, len = dictList.length; i < len; i++) {
+		try {
+			const result = await require('./lib/' + dictList[i])(word);
+			spinner.succeed(result);
+			break;
+		} catch (err) {
+			if (i + 1 === len) {
+				spinner.fail('查询失败');
 			}
-		} else {
-			i < dictList.length - 1 ? crawl(++i) : console.log(color.red('查询失败', true));
 		}
-	});
-})(0);
+	}
+})();
